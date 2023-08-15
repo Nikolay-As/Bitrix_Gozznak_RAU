63a4c86301c72b6b20ec5031853457407c5662d2
Николай Попурей <n.popurej@rauit.ru>
2023-08-15 11:22:11 +0300

    item 7.1 (Draft)

--- info/order_ticket/bronirovanie/index.php
+++ info/order_ticket/bronirovanie/index.php
@@ -6,19 +6,41 @@ $APPLICATION->SetTitle("Бронирование билетов");
 Выкупить билет в кассе Музея истории денег необходимо не позднее, чем за 10 минут до начала события. Опоздавшие к началу сеанса не будут допущены до участия в мероприятии.
 </p>
 <body>
-<div class="buy-banner-wrapper">
-	<div class="buy-banner-main">
-		<div class="text-content-wrapper">
-			<span class="banner-buy-title">Экскурсии и программы по расписанию&nbsp;&nbsp;&nbsp;&nbsp;</span><a href="https://museum.goznak.ru/info/order_ticket/bronirovanie/bronirovanie1.php" class="banner-buy-button"><span class="buy-button-text">Перейти</span></a>
-		</div>
-	</div>
-</div> 
- <br>
-<div class="buy-banner-wrapper">
-	<div class="buy-banner-main">
-		<div class="text-content-wrapper">
-	 		<span class="banner-buy-title">Участие в мастер-классе&nbsp;&nbsp;&nbsp;&nbsp;</span><a href="https://museum.goznak.ru/info/order_ticket/bronirovanie/bronirovanie2.php" class="banner-buy-button"><span class="buy-button-text">Перейти</span></a>
-		</div>
-	</div>
+<?php // Попурей НД РАУ 46444
+global $DB;
+?>
+
+<?php
+$sql = "SELECT * FROM skud_bd.application_for_excursions where status = 'По расписанию' and type =  'Обзорная экскурсия по музею' and amount_for_bron > 0";
+$Application_Query = $DB->Query($sql);
+
+if (count($Application_Query) > 0 and $Application_Query->fetch()['id'] > 0){
+  ?>
+  <div class="buy-banner-wrapper">
+  <div class="buy-banner-main">
+    <div class="text-content-wrapper">
+      <span class="banner-buy-title">Экскурсии и программы по расписанию&nbsp;&nbsp;&nbsp;&nbsp;</span><a href="https://museum.goznak.ru/info/order_ticket/bronirovanie/bronirovanie1.php" class="banner-buy-button"><span class="buy-button-text">Перейти</span></a>
+    </div> 
+  </div>
 </div>
+<?php } ?>
+
+<br>
+
+<?php
+$sql = "SELECT * FROM skud_bd.application_for_excursions where status = 'По расписанию' and type =  'Участие в мастер-классе по отливу бумаги' and amount_for_bron > 0";
+$Application_Query = $DB->Query($sql);
+
+if (count($Application_Query) > 0 and $Application_Query->fetch()['id'] > 0){
+  ?>
+  <div class="buy-banner-wrapper">
+  <div class="buy-banner-main">
+    <div class="text-content-wrapper">
+       <span class="banner-buy-title">Участие в мастер-классе&nbsp;&nbsp;&nbsp;&nbsp;</span><a href="https://museum.goznak.ru/info/order_ticket/bronirovanie/bronirovanie2.php" class="banner-buy-button"><span class="buy-button-text">Перейти</span></a>
+    </div>
+  </div>
+</div>
+<?php } ?>
+
+
 <?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
